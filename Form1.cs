c6a1de706b8d16b49581d8fd094d3b367356b0a1
MrRESJ <sethjredden@gmail.com>
2026-04-21 18:47:24 -0400

    dupe

--- Form1.cs
+++ Form1.cs
@@ -393,7 +393,11 @@ namespace Busy_Light
             public class tokens
             {
                 public string access_token { get; set; }
+                public int expires_in { get; set; }
                 public string refresh_token { get; set; }
+                public int refresh_token_expires_in { get; set; }
+                public string scope { get; set; }
+                public string token_type { get; set; }
             }
             public tokens Load()
             {
